25675a9f3360a8970db7557736ebf7a33066d9ba
Ethan Roberts <ejrobe@gmail.com>
2019-01-08 22:17:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,6 +88,7 @@ programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
+  programmer_hash
 end
 
 def changing_alan
@@ -110,6 +111,7 @@ def changing_alan
      alans_new_info = "GUI"
      
 programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -130,4 +132,5 @@ def adding_to_dennis
      }
 
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
